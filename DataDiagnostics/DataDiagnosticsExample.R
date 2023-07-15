a1a90b47ac6d149c2c6ebbd7ed4c8c0d3582d30d
NACHC-CAD <greshje.nachc@gmail.com>
2023-07-15 12:53:06 -0400

    adding some documents

---
+++ DataDiagnostics/DataDiagnosticsExample.R
@@ -0,0 +1,20 @@
+#
+# r-version
+#
+
+R.Version()
+
+#
+# installs
+#
+
+remotes::install_github("OHDSI/CohortGenerator")
+remotes::install_github("OHDSI/DbDiagnostics")
+
+#
+# libraries
+#
+
+library(DbDiagnostics)
+
+
